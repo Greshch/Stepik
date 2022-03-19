e020835111f52bfa0342087cfaf76e18506707e3
Greshch <greshchook@gmail.com>
2022-03-19 23:16:29 +0200

    1.8 Введение в синтаксис C++, часть 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 using namespace std;
 
-
-
 int main()
 {
-    
+    // put your code here
+    int T = 0;
+    std::cin >> T;
+    int a = 0;
+    int b = 0;
+    for (int i = 0; i < T; i++) {
+        std::cin >> a >> b;
+        std::cout << (a + b) << std::endl;
+    }
     return 0;
 }
